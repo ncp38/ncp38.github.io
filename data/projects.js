007842b1eb1c6c5f62297f16e81ee2305330d847
Nate Phillips <anemailanemailmykingdom@gmail.com>
2025-12-13 00:20:14 -0600

    Update projects.js

--- data/projects.js
+++ data/projects.js
@@ -5,7 +5,7 @@ const projects = [
     description: "Short description of the project.",
     keywords: ["machine learning", "vision", "python"],
     year: 2024,
-    link: "projects/project-1.html"
+    link: "projects/project1.html"
   },
   {
     id: "project-2",
@@ -13,6 +13,6 @@ const projects = [
     description: "Another project description.",
     keywords: ["web", "javascript"],
     year: 2023,
-    link: "projects/project-2.html"
+    link: "projects/project2.html"
   }
 ];

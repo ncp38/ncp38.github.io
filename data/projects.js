95d404a068b36666cff87b868ba0369dce056db8
Nate Phillips <anemailanemailmykingdom@gmail.com>
2025-12-13 13:41:47 -0600

    Facet Cards

--- data/projects.js
+++ data/projects.js
@@ -3,7 +3,7 @@ const projects = [
     id: "project-1",
     title: "Project Title One",
     description: "Short description of the project.",
-    keywords: ["machine learning", "vision", "python"],
+    keywords: ["machine learning", "vision", "python", "C#"],
     year: 2024,
     link: "projects/project1.html" 
   },
@@ -11,7 +11,7 @@ const projects = [
     id: "project-2",
     title: "Project Title Two",
     description: "Another project description.",
-    keywords: ["web", "javascript"],
+    keywords: ["web", "javascript", "c", "c++", "java"],
     year: 2023,
     link: "projects/project2.html"
   }

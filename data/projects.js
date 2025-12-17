5deaf71e84e191ab43fe65bab658be863b119ad1
Nate Phillips <anemailanemailmykingdom@gmail.com>
2025-12-17 13:50:44 -0600

    Updates

--- data/projects.js
+++ data/projects.js
@@ -1,18 +1,20 @@
+import { PROJ1TITLE, PROJ1KEYWORDS, PROJ1YEAR, PROJ1DESCRIPTION } from './constants.js';
+
 const projects = [
   {
-    id: "project-1",
+    id: "project2",
     title: "Project Title One",
     description: "Short description of the project.",
     keywords: ["machine learning", "vision", "python", "C#"],
     year: 2024,
-    link: "projects/project1.html" 
+    link: "projects/project2.html" 
   },
   {
-    id: "project-2",
-    title: "Project Title Two",
-    description: "Another project description.",
-    keywords: ["web", "javascript", "c", "c++", "java"],
-    year: 2023,
-    link: "projects/project2.html"
+    id: "project1",
+    title: PROJ1TITLE,
+    description: PROJ1DESCRIPTION,
+    keywords: PROJ1KEYWORDS,
+    year: PROJ1YEAR,
+    link: "projects/project1.html"
   }
 ];
---
+++ projects/constants.js
@@ -0,0 +1,6 @@
+// Constants for use in the project subfolder.
+
+export const PROJ1TITLE = "Visualization of Hurricane Data";
+export const PROJ1KEYWORDS = ["web", "javascript", "c", "c++", "java"];
+export const PROJ1YEAR = 2011;
+export const PROJ1DESCRIPTION = "Description";

d2997f85c82b166b84359ba2279c0d57df274dbd
Nate Phillips <anemailanemailmykingdom@gmail.com>
2025-12-17 15:25:31 -0600

    Update projects.js

--- data/projects.js
+++ data/projects.js
@@ -1,6 +1,6 @@
 //import { PROJ1TITLE, PROJ1KEYWORDS, PROJ1YEAR, PROJ1DESCRIPTION } from '../projects/constants.js';
 
-const module = await import('../projects/constants.js');
+//const module = await import('../projects/constants.js');
 
 const projects = [
   {
@@ -13,10 +13,10 @@ const projects = [
   },
   {
     id: "project1",
-    title: module.PROJ1TITLE,
-    description: module.PROJ1DESCRIPTION,
-    keywords: module.PROJ1KEYWORDS,
-    year: module.PROJ1YEAR,
+    title: "Working title 1",
+    description: "Description",
+    keywords: ["project keywords"],
+    year: 2011,
     link: "projects/project1.html"
   }
 ];

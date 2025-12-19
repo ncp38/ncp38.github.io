854810a920e5bf20a1ec2b865fbd00af1e93caf8
Nate Phillips <anemailanemailmykingdom@gmail.com>
2025-12-19 14:03:23 -0600

    Constants

--- data/projects.js
+++ data/projects.js
@@ -2,21 +2,33 @@
 
 //const module = await import('../projects/constants.js');
 
+// Constants for use in the project subfolder.
+
+const PROJ1TITLE = "Visualization of Hurricane Data";
+const PROJ1KEYWORDS = ["web", "javascript", "c", "c++", "java"];
+const PROJ1YEAR = 2011;
+const PROJ1DESCRIPTION = "Description";
+
+const PROJ2TITLE = "Working Title 2";
+const PROJ2KEYWORDS = ["c#"];
+const PROJ2YEAR = 2011;
+const PROJ2DESCRIPTION = "Description";
+
 const projects = [
   {
     id: "project2",
-    title: "Project Title One",
-    description: "Short description of the project.",
-    keywords: ["machine learning", "vision", "python", "C#"],
-    year: 2024,
+    title: PROJ2TITLE,
+    description: PROJ2DESCRIPTION,
+    keywords: PROJ2KEYWORDS,
+    year: PROJ2YEAR,
     link: "projects/project2.html" 
   },
   {
     id: "project1",
-    title: "Working title 1",
-    description: "Description",
-    keywords: ["project keywords"],
-    year: 2011,
+    title: PROJ1TITLE,
+    description: PROJ1DESCRIPTION,
+    keywords: PROJ1KEYWORDS,
+    year: PROJ1YEAR,
     link: "projects/project1.html"
   }
 ];

28f607c05c43876b4914fc572046494c7c1a1768
Nate Phillips <anemailanemailmykingdom@gmail.com>
2026-02-13 00:29:05 -0600

    Update site.js

--- assets/site.js
+++ assets/site.js
@@ -209,7 +209,7 @@ function render() {
   const oldCards = Array.from(results.children);
 
   var standardLinkBlock = '<h3><a href="' + p.link + '.pdf?ref=' + currentFilters + '">' + p.title + '&nbsp(' + p.year + ') </a></h3>';
-  var southeastconLinkBlock = '<h3><a href="' + p.link + 'ieeesoutheastcon2011presentation.pdf?ref=' + currentFilters + '">' + p.title + '&nbsp(' + p.year ') </a></h3>';
+  var southeastconLinkBlock = '<h3><a href="' + p.link + 'ieeesoutheastcon2011presentation.pdf?ref=' + currentFilters + '">' + p.title + '&nbsp(' + p.year + ') </a></h3>';
 
   var standardCodeLink = '<a href="' + p.link + '.txt">Code</a>';
   var southeastconCodeLink = '<a href="' + p.link + '/ieeesoutheastcon2011report.pdf">Report</a>';

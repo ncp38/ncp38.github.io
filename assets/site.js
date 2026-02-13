92d9c10859275623df48cc8f816107b44173c2c9
Nate Phillips <anemailanemailmykingdom@gmail.com>
2026-02-13 00:36:02 -0600

    Update site.js

--- assets/site.js
+++ assets/site.js
@@ -208,11 +208,7 @@ function renderCompoundFilters() {
 function render() {
   const oldCards = Array.from(results.children);
 
-  var standardLinkBlock = '<h3><a href="' + p.link + '.pdf?ref=' + currentFilters + '">' + p.title + '&nbsp(' + p.year + ') </a></h3>';
-  var southeastconLinkBlock = '<h3><a href="' + p.link + 'ieeesoutheastcon2011presentation.pdf?ref=' + currentFilters + '">' + p.title + '&nbsp(' + p.year + ') </a></h3>';
-
-  var standardCodeLink = '<a href="' + p.link + '.txt">Code</a>';
-  var southeastconCodeLink = '<a href="' + p.link + '/ieeesoutheastcon2011report.pdf">Report</a>';
+  
   // Animate out existing cards
   oldCards.forEach(card => card.classList.add("exit"));
   
@@ -224,6 +220,12 @@ function render() {
 	  if (toggleProjects.checked) {
 	  projects.filter(matchesFilters).forEach(p => {
 		const currentFilters = encodeURIComponent(window.location.search || "");
+		
+		var standardLinkBlock = '<h3><a href="' + p.link + '.pdf?ref=' + currentFilters + '">' + p.title + '&nbsp(' + p.year + ') </a></h3>';
+		var southeastconLinkBlock = '<h3><a href="' + p.link + 'ieeesoutheastcon2011presentation.pdf?ref=' + currentFilters + '">' + p.title + '&nbsp(' + p.year + ') </a></h3>';
+
+		var standardCodeLink = '<a href="' + p.link + '.txt">Code</a>';
+		var southeastconCodeLink = '<a href="' + p.link + '/ieeesoutheastcon2011report.pdf">Report</a>';
 		
 		//Standard Card clickable link and code-specific link
 		let linkBlock = standardLinkBlock;

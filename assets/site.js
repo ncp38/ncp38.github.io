b20458f6bfc68b19b74d0898375b069d82f18bf1
Nate Phillips <anemailanemailmykingdom@gmail.com>
2026-02-19 09:26:19 -0600

    Update site.js

--- assets/site.js
+++ assets/site.js
@@ -224,7 +224,7 @@ function render() {
 		//var standardLinkBlock = '<h3><a href="' + p.link + '.pdf?ref=' + currentFilters + '">' + p.title + '&nbsp(' + p.year + ') </a></h3>';
 		var standardLink = p.link;
 		var southeastconLink =  p.link + '/ieeesoutheastcon2011presentation.pdf';
-		var visualInterrogationLink = 'atoolforrapidvisualinterrogation.daybreakeducation.com';
+		var visualInterrogationLink = 'https://www.atoolforrapidvisualinterrogation.daybreakeducation.com';
 		
 		var standardLinkBlock = '<h3><a href="' + standardLink + '">' + p.title + '&nbsp(' + p.year + ') </a></h3>';
 		var southeastconLinkBlock = '<h3><a href="' + southeastconLink + '">' + p.title + '&nbsp(' + p.year + ') </a></h3>';

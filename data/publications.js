def8b9f0986234edaa2159c90d1ad1db52827e6c
Nate Phillips <anemailanemailmykingdom@gmail.com>
2026-03-28 22:53:35 -0500

    Update publications.js

--- data/publications.js
+++ data/publications.js
@@ -30,9 +30,9 @@ const PUB5KEYWORDS = ["Web Design", "Visualization", "Multivariate Data"];
 const PUB5YEAR = 2013;
 const PUB5DESCRIPTION = "Design and creation of a website that displays certain hydrological data from the Mobile Bay area: flow, salinity, temperature, position, and timestep. In this visualization, data points are located where they occur geographically. From each data point an arrow is displayed that points in the direction of the flow, with the length of the arrow representing the flow magnitude. Color is used to represent either salinity or temperature, based on a display toggle.";
 
-const PUB6TITLE = "Evidence that Eye Dominance Does Not Effect the Perceived Location of Virtual Objects in Optical See Through Augmented Reality";
-const PUB6KEYWORDS = ["Augmented Reality", "Perception"];
-const PUB6YEAR = 2025;
+const PUB6TITLE = "A Conceptual Replication and Extension of Triangulation by Walking for Measuring Perceived Distance Through a Wall";
+const PUB6KEYWORDS = ["Augmented Reality", "Perception", "Triangulation by Walking", "Replication"];
+const PUB6YEAR = 2022;
 const PUB6AUTHORS = "Nate Phillips, Farzana Alam Khan, Mohammed Safayet Arefin, Cindy L. Bethel, Jeanine Stetanucci, and J. Edward Swan";
 const PUB6VENUE = "2022 IEEE International Symposium on Mixed and Augmented Reality Adjunct";
 const PUB6DESCRIPTION = "Triangulation by walking is a method that has been used to measure perceived distance, where observers walk a triangular path. This method has been used at action space distances of approximately 1.5 to 30 meters. In this work, a conceptual replication of these triangulation by walking methods are discussed and evaluated for use in measuring the perceived distance of an object seen through a window set into a wall. The motivation for this work is to use triangulation by walking to study how perceived distance operates when augmented reality (AR) is used to visualize objects located behind opaque surfaces, in an AR application termed “x-ray vision.” This paper reports on experiences replicating an implementation of triangulation by walking as reported by Fukusima, Da Silva, and Loomis (1997). Their method was conceptually replicated in both outdoor and indoor settings, and the method was further extended to measure perceived distances of objects seen through a wall. These extensions are discussed in some detail, focusing on the modifications to the triangulation by walking method as well as the ramifications of these changes. Problems arising from using triangular geometry in calculations of perceived target locations are also introduced, and an alternate method is proposed that works to diminish the problematic effects.";
@@ -46,7 +46,7 @@ const publications = [
     venue: DISSVENUE,
     year: DISSYEAR,
     keywords: DISSKEYWORDS,
-    pdf: "https://scholarsjunction.msstate.edu/td/5612/"
+    link: "https://scholarsjunction.msstate.edu/td/5612/"
   },
   {
     id: "paper6",
@@ -56,7 +56,55 @@ const publications = [
 	venue: PUB6VENUE,
     keywords: PUB6KEYWORDS,
     year: PUB6YEAR,
-    pdf: "https://ieeexplore.ieee.org/iel7/9973799/9974160/09974279.pdf?casa_token=VCKzau_5XZsAAAAA:aMp5198pZPZ8RIPYBqPovvFTINPMJrqXnqHdFGg70U88HQWDn-rXIwYGd8U20_Uwylo0k8C0Lg" 
+    link: "https://ieeexplore.ieee.org/iel7/9973799/9974160/09974279.pdf?casa_token=VCKzau_5XZsAAAAA:aMp5198pZPZ8RIPYBqPovvFTINPMJrqXnqHdFGg70U88HQWDn-rXIwYGd8U20_Uwylo0k8C0Lg" 
+  },
+  {
+    id: "paper5",
+    title: PUB5TITLE,
+    description: PUB5DESCRIPTION,
+    keywords: PUB5KEYWORDS,
+    year: PUB5YEAR,
+    link: "papers/paper5" 
+  },
+  {
+    id: "paper4",
+    title: PUB4TITLE,
+    description: PUB4DESCRIPTION,
+    keywords: PUB4KEYWORDS,
+    year: PUB4YEAR,
+    link: "papers/paper4" 
+  },
+  {
+    id: "paper3",
+    title: PUB3TITLE,
+    description: PUB3DESCRIPTION,
+    keywords: PUB3KEYWORDS,
+    year: PUB3YEAR,
+    link: "papers/paper3" 
+  },
+  {
+    id: "paper2",
+    title: PUB2TITLE,
+    description: PUB2DESCRIPTION,
+    keywords: PUB2KEYWORDS,
+    year: PUB2YEAR,
+    link: "papers/aToolForRapidVisualInterrogation" 
+  },
+  {
+    id: "paper1",
+    title: PUB1TITLE,
+    description: PUB1DESCRIPTION,
+    keywords: PUB1KEYWORDS,
+    year: PUB1YEAR,
+    link: "papers/visualizationOfHurricaneData/index"
+  },
+  {
+    id: "paper0",
+    title: PUB0TITLE,
+    description: PUB0DESCRIPTION,
+    keywords: PUB0KEYWORDS,
+    year: PUB0YEAR,
+    link: "papers/ieeesoutheastcon2011"
   }
 ];
 

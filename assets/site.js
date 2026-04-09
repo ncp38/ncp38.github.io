f0a4814928092a0e3be560a6bc7d46cd3d5456c3
Nate Phillips <anemailanemailmykingdom@gmail.com>
2026-04-08 23:44:25 -0500

    Update site.js

--- assets/site.js
+++ assets/site.js
@@ -226,16 +226,19 @@ function render() {
 		var southeastconLink =  p.link + '/ieeesoutheastcon2011presentation.pdf';
 		var visualInterrogationLink = 'https://www.projects.daybreakeducation.com/aToolForRapidVisualInterrogation/alerts.php';
 		var gyrocopterCageMatchLink = p.link + '/finalProject.html';
+		var radialLink = p.link + '/finalProject.html';
 		
 		var standardLinkBlock = '<h3><a href="' + standardLink + '">' + p.title + '&nbsp(' + p.year + ') </a></h3>';
 		var southeastconLinkBlock = '<h3><a href="' + southeastconLink + '">' + p.title + '&nbsp(' + p.year + ') </a></h3>';
 		var visualInterrogationLinkBlock = '<h3><a href="' + visualInterrogationLink + '">' + p.title + '&nbsp(' + p.year + ') </a></h3>';
 		var gyrocopterCageMatchLinkBlock = '<h3><a href="' + gyrocopterCageMatchLink + '">' + p.title + '&nbsp(' + p.year + ') </a></h3>';
+		var radialLinkBlock = '<h3><a href="' + radialLink + '">' + p.title + '&nbsp(' + p.year + ') </a></h3>';
 
 		var standardCodeLink = '<a href="' + p.link + '.txt">Code</a>';
 		var southeastconCodeLink = '<a href="' + p.link + '/ieeesoutheastcon2011report.pdf">Report</a>';
 		var visualInterrogationCodeLink = 'Code: <a href="' + p.link + '/alerts.txt">alerts.php</a>&nbsp<a href="' + p.link + '/query_alerts.txt">query_alerts.php</a>&nbsp<a href="' + p.link + '/alertlist.txt">alertlist.js</a>';
 		var gyrocopterCageMatchCodeLink = 'Code: <a href="' + p.link + '/gyrocopterCageMatch.txt">index.html</a>&nbsp<a href="' + p.link + '/gyrocopterCageMatch.pdf">Presentation</a>';
+		var radialCodeLink = 'Code: <a href="' + p.link + '/radialDataVisualization.txt">index.html</a>&nbsp<a href="' + p.link + '/projectPaper.pdf">Paper</a>&nbsp<a href="' + p.link + '/projectPresentation.pdf">Presentation</a>';
 		
 		//Standard Card clickable link and code-specific link
 		let linkBlock = standardLinkBlock;
@@ -260,6 +263,12 @@ function render() {
 			directLink = gyrocopterCageMatchLink;
 			linkBlock = gyrocopterCageMatchLinkBlock;
 			linkToCodeBlock = gyrocopterCageMatchCodeLink;
+		}
+		else if(p.title == "Radial Data Visualization")
+		{
+			directLink = radialMatchLink;
+			linkBlock = radialMatchLinkBlock;
+			linkToCodeBlock = radialMatchCodeLink;
 		}
 		const card = createCard(`
 		  <div class="card">
